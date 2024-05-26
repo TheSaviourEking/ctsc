02c986f51c4c49594f8c73bbc491116d37e9e2d1
TheSaviourEking <saviour.eking@outlook.com>
2024-05-26 02:23:36 +0100

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // Enable source maps in the production build
+  },
   server: {
     port: 3003,
     proxy: {
