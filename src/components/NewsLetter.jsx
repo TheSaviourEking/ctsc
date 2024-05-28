5cea554288196e93594d363afbd66ed291a20a23
TheSaviourEking <saviour.eking@outlook.com>
2024-05-28 23:23:59 +0100

    refactor: refactor error handling on form invalid details

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -5,6 +5,7 @@ import './NewsLetter.css';
 
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import FormInput from './FormInput';
 
 const NewsLetter = () => {
     const [userDetails, setUserDetails] = useState({
@@ -13,13 +14,18 @@ const NewsLetter = () => {
         email: ''
     });
 
-    const [errors, setErrors] = useState({});
+    const inputs = [
+        { id: 'firstName', name: 'firstname', placeholder: "Enter your first name", type: 'text', required: true, errorMessage: 'First name must be between 3 and 30 characters', pattern: "^[A-Za-z]{3,30}$" },
+        { id: "lastName", name: 'lastname', placeholder: "Enter your last name", type: 'text', required: true, errorMessage: 'First name must be between 3 and 30 characters', pattern: "^[A-Za-z]{3,30}$" },
+        { id: 'email', name: 'email', placeholder: "Enter your email address", type: 'email', required: true, errorMessage: 'Email is required' }
+    ]
+
     const [safeDetails, setSafeDetails] = useState(false);
 
-    const API_BASE_URL = import.meta.env.VITE_API_URL;
     useEffect(() => {
         if (safeDetails) {
             (async function () {
+                const API_BASE_URL = import.meta.env.VITE_API_URL;
                 const { firstName, lastName, email } = userDetails;
                 const request = await fetch(`${API_BASE_URL}/newsletter/`, {
                     method: 'POST',
@@ -33,6 +39,7 @@ const NewsLetter = () => {
                 });
                 const response = await request.json();
                 if (request.ok) {
+                    alert(request)
                     setUserDetails((prevDetails) => ({ ...prevDetails, firstName: '', lastName: '', email: '' }));
                 }
                 toast(response);
@@ -40,24 +47,9 @@ const NewsLetter = () => {
         }
     }, [safeDetails]);
 
-    const fixErrors = () => {
-        const newErrors = validateInput(userDetails);
-        setErrors(newErrors);
+    const handleInputChange = (e) => {
+        setUserDetails((prevDetails) => ({ ...prevDetails, [e.target.id]: e.target.value }));
     }
-    const handleFirstNameChange = (event) => {
-        fixErrors();
-        setUserDetails((prevDetails) => ({ ...prevDetails, firstName: event.target.value }));
-    };
-
-    const handleLastNameChange = (event) => {
-        setUserDetails((prevDetails) => ({ ...prevDetails, lastName: event.target.value }));
-        fixErrors();
-    };
-
-    const handleEmailChange = (event) => {
-        setUserDetails((prevDetails) => ({ ...prevDetails, email: event.target.value }));
-        fixErrors();
-    };
 
     const validateInput = (inputData) => {
         const errors = {};
@@ -65,6 +57,7 @@ const NewsLetter = () => {
             errors.email = 'Invalid email address';
         }
         if (!validator.isLength(inputData.firstName, { min: 2, max: 30 })) {
+            console.log(inputData.firstName,)
             errors.firstName = 'First name must be between 2 and 30 characters';
         }
         if (!validator.isLength(inputData.lastName, { min: 2, max: 30 })) {
@@ -83,13 +76,20 @@ const NewsLetter = () => {
         };
         const errors = validateInput(inputData);
         if (Object.keys(errors).length > 0) {
-            // console.log(errors);
+            console.log(errors);
         } else {
+            alert('safeDetails')
             setSafeDetails(() => true);
         }
     };
 
+    // const handleFormSubmit = (event) => {
+    //     event.preventDefault();
 
+    //     console.error(userDetails);
+    // }
+
+    console.log(userDetails)
     return (
         <section className="container-fluid signup-section">
             <div className="container content-section">
@@ -101,22 +101,29 @@ const NewsLetter = () => {
             <div className="row justify-content-center">
                 <div className="col-10 col-md-6">
 
-                    <form id="signupForm" className="signup" onSubmit={(e) => handleFormSubmit(e)}>
+                    {/* <form id="signupForm" className="signup" onSubmit={(e) => handleFormSubmit(e)}>
                         <div className="input-wrapper">
                             {errors.firstName && <span style={{ color: 'red' }}>{errors.firstName}</span>}
                             <input type="text" onChange={(e) => handleFirstNameChange(e)} value={userDetails.firstName} id="first-name" className="form-control input-style mb-4" placeholder="Enter your first name"
                                 required />
-                            {/* <span id="firstNameError" className="error"></span> */}
 
                             {errors.lastName && <div style={{ color: 'red' }}>{errors.lastName}</div>}
                             <input type="text" onChange={(e) => handleLastNameChange(e)} value={userDetails.lastName} id="last-name" className="form-control input-style mb-4" placeholder="Enter your last name"
                                 required />
-                            {/* <span id="lastNameError" className="error"></span> */}
 
                             {errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
                             <input type="email" onChange={(e) => handleEmailChange(e)} value={userDetails.email} id="email" className="form-control input-style mb-4" placeholder="Enter your email address"
                                 required />
 
+                            <button type="submit" className="button btn-primary signup-btn">Shine Your Light</button>
+                        </div>
+                    </form> */}
+
+                    <form id="signupForm" className="signup" onSubmit={(e) => handleFormSubmit(e)}>
+                        <div className="input-wrapper">
+                            {inputs.map((input, i) => (
+                                <FormInput key={i} {...input} userDetail={userDetails[input.id]} onChange={handleInputChange} />
+                            ))}
                             <button type="submit" className="button btn-primary signup-btn">Shine Your Light</button>
                         </div>
                     </form>
